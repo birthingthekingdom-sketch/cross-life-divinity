124647904a814ee2244d873677b8547a147acd64
birthingthekingdom-sketch <birthingthekingdom@gmail.com>
2026-02-21 12:14:14 -0600

    Update server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const serverless = require('serverless-http');
 
 // Import your main Express app
-// Adjust this path to where your main app file is located
 const app = require('../../dist/public/index.js');
 
 // Create a serverless handler
-exports.handler = serverless(app); 
+exports.handler = serverless(app);
